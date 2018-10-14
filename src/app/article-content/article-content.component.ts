5746605905605cd6fc6c58491fd7848ab0f95bb6
Stefan Prasse <prasse.stefan@gmx.de>
2018-10-14 22:08:31 +0200

    overview layout changes

--- src/app/article-content/article-content.component.ts
+++ src/app/article-content/article-content.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'ttrss-article-content',
   templateUrl: './article-content.component.html',
-  styleUrls: ['./article-content.component.scss']
+  styleUrls: ['./article-content.component.css']
 })
 export class ArticleContentComponent implements OnInit {
 
