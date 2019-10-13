5dd5717452d7a8a03effd5d11a4c63f4b203fc05
Stefan Prasse <prasse.stefan@gmx.de>
2019-10-13 22:45:03 +0200

    angular update

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,27 +1,25 @@
 import { NgModule } from '@angular/core';
 import {CdkTableModule} from '@angular/cdk/table';
-import {
-  MatInputModule,
-  MatFormFieldModule,
-  MatButtonModule,
-  MatSidenavModule,
-  MatToolbarModule,
-  MatIconModule,
-  MatListModule,
-  MatMenuModule,
-  MatChipsModule,
-  MatOptionModule,
-  MatSelectModule,
-  MatButtonToggleModule,
-  MatCheckboxModule,
-  MatDialogModule,
-  MatExpansionModule,
-  MatTooltipModule,
-  MatTabsModule,
-  MatTreeModule,
-  MatSlideToggleModule,
-  MatProgressBarModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTreeModule } from '@angular/material/tree';
 
 @NgModule({
   imports: [
