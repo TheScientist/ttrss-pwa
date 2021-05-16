3373701057f5a5154ab67a8d7197e4437cc9c572
TheScientist <prasse.stefan@gmx.de>
2021-05-16 11:12:35 +0200

    fix lint

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -19,7 +19,3 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-//  (window as any).global = window;
-//  global.Buffer = global.Buffer || require('buffer').Buffer;
-//  import * as process from 'process';
-//  window['process'] = process;
